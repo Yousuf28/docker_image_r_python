d0c6edf469c012ddfb3924d531a6fbb8218c0682
Yousuf Ali <yousuf.pharma@gmail.com>
2024-05-14 11:57:02 -0400

    update R version

--- .devcontainer/build_docker.sh
+++ .devcontainer/build_docker.sh
@@ -2,20 +2,20 @@
 
 echo "Build the docker"
 
-docker build . -f Dockerfile --no-cache \
+docker build . -f Dockerfile \
                --progress=plain \
                --build-arg PROJECT_NAME="R Dev Env" \
                --build-arg VENV_NAME="R_ENV" \
                --build-arg R_VERSION_MAJOR=4 \
-               --build-arg R_VERSION_MINOR=3 \
-               --build-arg R_VERSION_PATCH=1 \
+               --build-arg R_VERSION_MINOR=4 \
+               --build-arg R_VERSION_PATCH=0 \
                 --build-arg DEBIAN_FRONTEND=noninteractive \
                  --build-arg CRAN_MIRROR="https://cran.rstudio.com/" \
-               -t yousuf28/r_python:pfda
+               -t yousuf28/pfda:test
 
 if [[ $? = 0 ]] ; then
 echo "Pushing docker..."
-docker push yousuf28/r_python:pfda
+# docker push yousuf28/pfda:test
 else
 echo "Docker build failed"
 fi
